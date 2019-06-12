8802a6cc584532750ba3dcedabd4fbd559ab522b
zhuqinb <649283893@qq.com>
2019-06-12 15:33:52 +0800

    添加评论

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,26 +37,36 @@ module.exports = {
     title: '前端',
     description: 'es6',
     base: '/note/',
-    // 假定 GitHub。也可以是一个完整的 GitLab 网址
-    repo: 'https://gitee.com/zhuqinb_admin/note.git',
-    // 如果你的文档不在仓库的根部
-    docsDir: 'web/docs',
-    // 可选，默认为 master
-    docsBranch: 'master',
-    // 默认为 true，设置为 false 来禁用
-    editLinks: true,
-    lastUpdated: 'Last Updated',
     head: [
         ['link', { rel: 'icon', href: '/img/logo.ico' }],
         ['link', { rel: 'manifest', href: '/manifest.json' }]
     ],
     themeConfig: {
+        // 假定 GitHub。也可以是一个完整的 GitLab 网址
+        repo: 'https://github.com/zhuqinb/note',
+        // 如果你的文档不在仓库的根部
+        docsDir: 'docs',
+        // 可选，默认为 master
+        docsBranch: 'master',
+        // 默认为 true，设置为 false 来禁用
+        editLinks: true,
+        editLinkText: '编辑此页面',
+        lastUpdated: '上次更新',
         nav: [{
             text: '技术栈',
             items: navs
         }],
         sidebar
-    }
+    },
+    plugins: [
+        ['@vuepress/back-to-top', true],
+        [
+            '@vuepress/register-components',
+            {
+                componentsDir: './components'
+            }
+        ]
+    ]
 }
 
 function genSidebarConfig(dir) {
---
+++ docs/tools/vuepress.md
@@ -0,0 +1,5 @@
+# vuepress
+
+https://juejin.im/post/5c2e0b2f5188257c30462a21
+
+生成默认主题 `vuepress eject [targetDir]`
